a56a86be6780086a0d7edd973966d0dd70d7c881
Tom Blazejewski <tom.blazejewski@mail.utoronto.ca>
2015-10-31 14:19:44 -0400

    It's iterating but appears to be outputting utter nonsense. Okay, well. That's still progress. It sorta looks like it's not really assigning clusters? The points are all at the same spot and that spot is roughly at the midpoints of the three distributions generating the data.

--- PGMs/gibbs_sampler.jl
+++ PGMs/gibbs_sampler.jl
@@ -8,15 +8,34 @@ println("Do GIBBS")
 using Distributions
 using PDMats
 
+function categorical_to_one_hot(z)
+  top_cat = maximum(z) #let's assume we start at 1, though.
+  #println(top_cat)
+  #println(length(z))
+  big_array = zeros(Int64, length(z), int(top_cat))
+  for val in 1:length(z)
+    big_array[val, z[val]] = 1
+  end
+  return big_array
+end
+
 function eq_23(sigma, lambda, n, z, x, k)
   #So we sample a mu_k given some info on observations and states.
   #I think we need to remove the data point we're conditioning on...?
+  #Right, right, I need to turn z into one-hot.
+  z = categorical_to_one_hot(z)
+  #println(z)
   n_k = sum(z[:,k])
-  x_bar_k = sum(x[:,k] .* x) / n_k
-  mu_hat_k = ((n_k / sigma^2) / ((n_k / sigma^2) + (1/lambda^2))) * x_bar_k
+  #println(x)
+  #println(x)
+  x_bar_k = sum(z[:,k] .* x, 1) / n_k
+  #println(x_bar_k)
+  mu_hat_k = ((n_k / sigma^2) / ((n_k / sigma^2) + (1/lambda^2))) .* x_bar_k
   lambda_hat_k = ((n_k / sigma^2) + (1/(lambda^2)))^-1
   #mu_hat_k, lambda_hat_k
-  my_gauss = MvNormal(mu_hat_k, lambda_hat_k)
+  #println("mu_hat_k ", mu_hat_k)
+  #println("lambda_hat_k ", lambda_hat_k)
+  my_gauss = MvNormal(vec(mu_hat_k), lambda_hat_k)
   return rand(my_gauss)
 end
 
@@ -25,7 +44,7 @@ function log_sum_exp(vec)
   the_max = maximum(vec)
   s = 0.0
   for i in 1:length(vec)
-    s += exp(x[i] - the_max)
+    s += exp(vec[i] - the_max)
   end
   return log(s) + the_max
 end
@@ -36,19 +55,22 @@ function eq_21(x, mus, sigma, K)
   #better things.
   #Summing the itsy bitsy probabilities here is where the log-sum-exp trick comes in to play.
   centers = MvNormal[]
-  mus = mus'
+  #println("mus: ", mus)
+  #mus = mus'
   for i in 1:K
-    println(mus[i, :])
+    #println(mus[i, :])
     push!(centers, MvNormal(vec(mus[i, :]), sigma))
   end
-  z = zeros(size(x)[2])
-  for i in 1:size(x)[2]
+  z = zeros(size(x)[1])
+  #println("size(x): ", size(x)[1])
+  for i in 1:size(x)[1]
     #for each point in x...
-    println("x ", x[i, :])
-    println(logpdf(centers[1], x[i, :]))
-    center_probs = [logpdf(centers[j], x[i, :]) for j in centers]
+    #println("x ", x[i, :])
+    #println(logpdf(centers[1], vec(x[i, :])))
+    center_probs = [logpdf(centers[j], vec(x[i, :])) for j in 1:K]
     partition = log_sum_exp(center_probs)
-    to_sample = Categorical([exp(a - partition) for a in center_probs])
+    #println([exp(a - partition) for a in center_probs])
+    to_sample = Categorical(float([exp(a - partition) for a in center_probs]))
     z[i] = rand(to_sample)
   end
   return z
@@ -56,7 +78,7 @@ end
 
 function shuffle_rows(data)
   println("Hello?")
-  rows = size(data)[2]
+  rows = size(data)[1]
   row_indices = [1:rows;]
   data = data[shuffle(row_indices), :]
   println("done")
@@ -74,31 +96,43 @@ function gen_data()
   #Why not do "each row is datapoint"? Going to transpose.
   samples = [rand(center_1, 250) rand(center_2, 250) rand(center_3, 250)]'
   println("Did generate...")
+  println(size(samples))
+  println(center_1)
+  println(center_2)
+  println(center_3)
   return shuffle_rows(samples) #want to randomize order.
 end
 
 function main()
   #Generate data.
   data = gen_data()
+  println("data: ", size(data))
   println("Starting")
   sigma = 1.0
   lambda = 0.5
   K = 3
   #Initiliaze mu's, where our centers start.
-  mu = rand(MvNormal(randn(2) * 0.01, 0.05), K)
+  mu = rand(MvNormal(randn(2) * 0.01, 0.05), K)'
   println("Past mu")
 
-  for count in 1:100 #100 iterations is just a place-holder for a more reasonable number of iterations in future.
-    println(count)
+  for count in 1:10000 #100 iterations is just a place-holder for a more reasonable number of iterations in future.
+    if count % 1000 == 0
+      println(count)
+    end
   #for i in 1:rows
     #eq_21 already goes for all rows...
     z = eq_21(data, mu, sigma, K)
   #end
+    mus = zeros(K, 2)
     for i in 1:K
       #sigma, lambda, n, z, x, k
       mu = eq_23(sigma, lambda, 750, z, data, K)
+      #println("Generated mu for $i: ", mu)
+      mus[i, :] = mu
     end
+    mu = mus
   end
+  println(mu)
 end
 
 #println(gen_data()[1:10, :])
