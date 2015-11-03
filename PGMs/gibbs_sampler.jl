e730d155c04c8a6e9d676fd4e5587fbd8ec64cae
tomblaze <tb2595@columbia.edu>
2015-11-02 23:13:23 -0500

    Okay, so basically there now. Just need to figure out how to incoporate probability of z into the 'total prob' calculation because I'm realizing that I'm not actually calculating joint probability the way I thought I was and this is clear from results. You may initiate write-up, though, for sure.

--- PGMs/gibbs_sampler.jl
+++ PGMs/gibbs_sampler.jl
@@ -7,7 +7,7 @@
 println("Do GIBBS")
 using Distributions
 using PDMats
-using Gadfly
+using PyPlot
 
 function categorical_to_one_hot(z)
   top_cat = maximum(z) #let's assume we start at 1, though.
@@ -51,6 +51,19 @@ function log_sum_exp(vec)
   return log(s) + the_max
 end
 
+function total_prob(x, z, mus, sigma, K)
+  centers = MvNormal[]
+  for i in 1:K
+    push!(centers, MvNormal(vec(mus[i, :]), sigma))
+  end
+  big_prob = 0
+  for i in 1:size(x)[1]
+    center_probs = [log(1/3) + logpdf(centers[j], vec(x[i, :])) for j in 1:K]
+    big_prob += sum(center_probs)
+  end
+  return big_prob
+end
+
 function eq_21(x, mus, sigma, K)
   #So I somehow need to sample from the distributions given some data point.
   #Not sure how to actually code this, which is a little annoying. Maybe tomorrow will bring
@@ -79,11 +92,9 @@ function eq_21(x, mus, sigma, K)
 end
 
 function shuffle_rows(data)
-  println("Hello?")
   rows = size(data)[1]
   row_indices = [1:rows;]
   data = data[shuffle(row_indices), :]
-  println("done")
   return data
 end
 
@@ -91,51 +102,88 @@ function gen_data()
   #Generate three multivariate
   sig = 1.0
   dims = 2 #I don't imagine this'll ever change...
-  center_1 = MvNormal(randn(dims), sig)
-  center_2 = MvNormal(randn(dims), sig)
-  center_3 = MvNormal(randn(dims), sig)
+  u_1 = randn(dims) * 3
+  u_2 = randn(dims) * 3
+  u_3 = randn(dims) * 3
+  center_1 = MvNormal(u_1, sig)
+  center_2 = MvNormal(u_2, sig)
+  center_3 = MvNormal(u_3, sig)
   #Sampling from MvNormal gives array like dims * n... that seems unnatural, right?
   #Why not do "each row is datapoint"? Going to transpose.
-  samples = [rand(center_1, 5000) rand(center_2, 5000) rand(center_3, 5000)]'
+  samples = [rand(center_1, 1000) rand(center_2, 1000) rand(center_3, 1000)]'
   println("Did generate...")
   println(size(samples))
   println(center_1)
   println(center_2)
   println(center_3)
-  return shuffle_rows(samples) #want to randomize order.
+  return u_1, u_2, u_3, shuffle_rows(samples) #want to randomize order.
+end
+
+function color_generator(n)
+  r = (255 * n) / 100
+  g = (255 * (100 - n)) / 100
+  b = 0
+  return r, g, b
 end
 
 function main()
   #Generate data.
-  data = gen_data()
+  rand_int = int(rand() * 10000)
+  #println("rand int:", rand_int)
+  #
+  srand(rand_int)
+  #srand(761)
+  #srand(9561)
+  #srand(6698)
+  #srand(4863)
+  #srand(5110) #genuinely doesn't quite get there...
+  #srand(4734) #oh my god, the chosen one!
+  u_1, u_2, u_3, data = gen_data()
   println("data: ", size(data))
+  scatter(data[:,1], data[:,2])
   println("Starting")
   sigma = 1.0
   lambda = 0.5
   K = 3
   #Initiliaze mu's, where our centers start.
   mu = rand(MvNormal(randn(2), 2), K)'
+
+  scatter(mu[:,1], mu[:,2], c="yellow", s=200)
+  scatter(u_1[1], u_1[2], c="white", s=300)
+  scatter(u_2[1], u_2[2], c="white", s=300)
+  scatter(u_3[1], u_3[2], c="white", s=300)
+
   println(mu)
   println("Past mu")
 
-  for count in 1:1000 #100 iterations is just a place-holder for a more reasonable number of iterations in future.
-    if count % 100 == 0
+  all_probs = Float64[]
+
+  num_iter = 50
+  for count in 1:num_iter #100 iterations is just a place-holder for a more reasonable number of iterations in future.
+    #data = shuffle_rows(data)
+    if count % 3 == 0
       println(count)
+      r, g, b=color_generator((count/num_iter) * 100)
+      scatter(mu[:,1], mu[:,2], c=@sprintf("#%02X%02X%02X", r, g, b), s=50)
     end
   #for i in 1:rows
     #eq_21 already goes for all rows...
     z = eq_21(data, mu, sigma, K)
+    push!(all_probs, total_prob(data, mu, sigma, K))
   #end
     mus = zeros(K, 2)
     for i in 1:K
       #sigma, lambda, n, z, x, k
-      mu = eq_23(sigma, lambda, 15000, z, data, i)
+      mu = eq_23(sigma, lambda, 3000, z, data, i)
       #println("Generated mu for $i: ", mu)
       mus[i, :] = mu
     end
     mu = mus
   end
   println(mu)
+  println("rand_int: ", rand_int)
+  close()
+  plot(1:num_iter, all_probs)
 end
 
 #println(gen_data()[1:10, :])
